9761553c8aa386e73a0895eef780f2bedba41227
capybara <tmanfdl@gmail.com>
2019-01-08 13:37:29 +0900

    add SearchingImageServiceManager
    retrofit 사용을 위한 manager 제작.

---
+++ app/src/main/java/com/example/capybara/findimage/network/SearchingImageServiceManager.kt
@@ -0,0 +1,39 @@
+package com.example.capybara.findimage.network
+
+import com.example.capybara.findimage.network.repo.ImageResultRepo
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class SearchingImageServiceManager(ADDRESS: String, private val AUTHORIZATION: String) {
+
+    var service: SearchingImageService
+
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(ADDRESS)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        service = retrofit.create<SearchingImageService>(SearchingImageService::class.java)
+    }
+
+    fun searchImage(searchText: String): Response<ImageResultRepo> {
+        return service.searchingImageResultRepo(AUTHORIZATION, searchText).execute()
+    }
+
+
+}
+
+fun <T> Response<T>.result(): T? {
+    return this.body()
+}
+
+fun <T> Response<T>.error(): ResponseBody? {
+    return this.errorBody()
+}
+
+fun ImageResultRepo?.size(): Any {
+    return this?.documents?.size ?: 0
+}
--- app/src/test/java/com/example/capybara/findimage/ExampleUnitTest.kt
+++ app/src/test/java/com/example/capybara/findimage/ExampleUnitTest.kt
@@ -1,66 +1,50 @@
 package com.example.capybara.findimage
 
-import com.example.capybara.findimage.network.SearchingImageService
+import com.example.capybara.findimage.network.SearchingImageServiceManager
+import com.example.capybara.findimage.network.error
+import com.example.capybara.findimage.network.result
+import com.example.capybara.findimage.network.size
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class ExampleUnitTest {
     val SEARCHING_IMAGE_ADDRESS = "https://dapi.kakao.com/"
     val AUTHORIZATION = "KakaoAK 74d08370ac9ea1260a9a56d1e4ea25ad"
     val searchText = "설현"
 
-    lateinit var service: SearchingImageService
+    lateinit var manager: SearchingImageServiceManager
 
     @Before
     fun before() {
-        val retrofit = Retrofit.Builder()
-            .baseUrl(SEARCHING_IMAGE_ADDRESS)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        service = retrofit.create<SearchingImageService>(SearchingImageService::class.java)
-
+        manager = SearchingImageServiceManager(SEARCHING_IMAGE_ADDRESS, AUTHORIZATION)
     }
 
     @Test
     fun testAPI() {
-        val response = service.searchingImageResultRepo(AUTHORIZATION, searchText).execute()
-        val body = response.body()
-        val title = body?.documents?.get(0)?.thumbnail_url ?: ""
+        val result = manager.searchImage(searchText).result()
+        val title = result?.documents?.get(0)?.thumbnail_url ?: ""
 
         assertEquals("is same title", "https://search4.kakaocdn.net/argon/130x130_85_c/EQa9bd9jV9t", title)
-
     }
 
     @Test
     fun testAPINoResult() {
-        val response = service.searchingImageResultRepo(AUTHORIZATION, "쌰탳ㅎ").execute()
-        val body = response.body()
-        val documents = body?.documents
-
-        assertEquals("is document count is zero", 0, documents?.size)
-
+        val size = manager.searchImage("쌰탳ㅎ").result().size()
+        assertEquals("is document count is zero", 0, size)
     }
 
     @Test
     fun testAPIError() {
-        val response = service.searchingImageResultRepo(AUTHORIZATION, "쌰탳ㅎ").execute()
-        val errorBody = response.errorBody()
-
+        val errorBody = manager.searchImage("쌰탳ㅎ").error()
         assertEquals("is error is null", null, errorBody)
-
     }
 
     @Test
     fun testAPIWithPage() {
-        val response = service.searchingImageResultRepo(AUTHORIZATION, searchText, 1).execute()
-        val body = response.body()
+        val body = manager.searchImage("쌰탳ㅎ").result()
         val isEnd = body?.meta?.is_end
-
-        assertEquals("is last page", false, isEnd)
-
+        assertEquals("is last page", true, isEnd)
     }
 }
+
