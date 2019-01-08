6c82ecbaf2c12f41fb08ddb8826eb3d8672ab8ec
capybara <tmanfdl@gmail.com>
2019-01-09 00:32:51 +0900

    update recyclerview aspectRatio
    recyclerView에 출력하는 이미지를 목표에 맞게 width는 Match_parent로 설정하고 height는 이미지 비율에 맞게 설정

--- app/src/main/java/com/example/capybara/findimage/ImageResultAdapter.kt
+++ app/src/main/java/com/example/capybara/findimage/ImageResultAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.capybara.findimage
 
 import android.content.Intent
+import android.graphics.drawable.Animatable
 import android.net.Uri
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -9,12 +10,14 @@ import android.view.ViewGroup
 import com.example.capybara.findimage.databinding.ItemImageBinding
 import com.example.capybara.findimage.network.repo.ImageRepo
 import com.example.capybara.findimage.network.repo.ImageResultRepo
+import com.facebook.drawee.backends.pipeline.Fresco
+import com.facebook.drawee.controller.ControllerListener
+import com.facebook.imagepipeline.image.ImageInfo
 import kotlinx.android.synthetic.main.item_image.view.*
 
 class ImageResultAdapter(
     private val imageResultRepo: ImageResultRepo, private val searchNextPage: View.OnClickListener
-) :
-    RecyclerView.Adapter<ImageResultAdapter.ImageViewHolder>() {
+) : RecyclerView.Adapter<ImageResultAdapter.ImageViewHolder>() {
 
     var page = 1
 
@@ -30,6 +33,11 @@ class ImageResultAdapter(
                         imageRepo = objects as ImageRepo
                         imageRepo?.image_url?.let { url ->
                             itemView.image.setImageURI(Uri.parse(url))
+                            itemView.image.controller = Fresco.newDraweeControllerBuilder()
+                                .setUri(url)
+                                .setControllerListener(controllerListener())
+                                .build()
+
                             itemView.image.setOnClickListener(this)
                         }
                     }
@@ -37,6 +45,35 @@ class ImageResultAdapter(
                 ITEM_VIEW_FOOTER -> {
                     if (imageResultRepo.meta.is_end) itemView.image.visibility = View.GONE
                     else itemView.image.setOnClickListener(searchNextPage)
+                }
+            }
+        }
+
+        private fun controllerListener(): ControllerListener<ImageInfo> {
+            return object : ControllerListener<ImageInfo> {
+                override fun onFailure(id: String?, throwable: Throwable?) {}
+
+                override fun onRelease(id: String?) {}
+
+                override fun onSubmit(id: String?, callerContext: Any?) {}
+
+                override fun onIntermediateImageSet(id: String?, imageInfo: ImageInfo?) {
+                    updateViewSize(imageInfo)
+                }
+
+                override fun onIntermediateImageFailed(id: String?, throwable: Throwable?) {}
+
+                override fun onFinalImageSet(id: String?, imageInfo: ImageInfo?, animatable: Animatable?) {
+                    updateViewSize(imageInfo)
+                }
+
+                private fun updateViewSize(imageInfo: ImageInfo?) {
+                    if (imageInfo != null) {
+                        itemView.image.layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT
+                        itemView.image.layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT
+                        itemView.image.aspectRatio = imageInfo.width.toFloat() /
+                                imageInfo.height
+                    }
                 }
             }
         }
@@ -89,4 +126,3 @@ class ImageResultAdapter(
         private const val ITEM_VIEW_FOOTER = 1
     }
 }
-
